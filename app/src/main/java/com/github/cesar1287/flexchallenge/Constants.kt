0df2b923f158d971580f5fb383c90ea7a456e3f9
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2021-04-14 22:06:10 -0300

    created constants and options to customize

---
+++ app/src/main/java/com/github/cesar1287/flexchallenge/Constants.kt
@@ -0,0 +1,9 @@
+package com.github.cesar1287.flexchallenge
+
+class Constants {
+
+    object TimerView {
+        const val ARC_START_ANGLE = 270f
+        const val THICKNESS_SCALE = 0.03f
+    }
+}
