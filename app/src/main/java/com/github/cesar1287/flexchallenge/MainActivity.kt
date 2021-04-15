3bb917c65c7b22cafb99d5891536937f40dac227
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2021-04-14 22:05:30 -0300

    implemented time view in the screen

--- app/src/main/java/com/github/cesar1287/flexchallenge/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/flexchallenge/MainActivity.kt
@@ -2,10 +2,21 @@ package com.github.cesar1287.flexchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.cesar1287.flexchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            timerView.setOnClickListener {
+                timerView.start(5L)
+            }
+        }
     }
 }
